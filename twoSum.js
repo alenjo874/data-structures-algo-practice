66f39c43fe853ad9537ddd89a6314c7821f506fe
Alen Jo <alenjo874@gmail.com>
2022-04-14 10:01:18 -0400

    two sum refactor

--- twoSum.js
+++ twoSum.js
@@ -1,6 +1,8 @@
 // https://leetcode.com/problems/two-sum/
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
+// ================================
+// BRUTE FORCE O(n^2)
 var twoSum = function (nums, target) {
   //iterate over nums array
   //hold first nums[i]
@@ -21,4 +23,22 @@ var twoSum = function (nums, target) {
   }
 
   return result.slice(0, 2);
+};
+// ================================
+//O(n) time and space
+var twoSum = function (nums, target) {
+  //iterate overs array, create hashmap
+  //if diff equals target return indexes
+  //else create new key/value
+
+  const hashMap = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const diff = target - nums[i];
+    if (diff in hashMap) {
+      return [hashMap[diff], i];
+    } else {
+      hashMap[nums[i]] = i;
+    }
+  }
 };
