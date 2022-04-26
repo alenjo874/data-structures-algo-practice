ab24461878017e630dda0168efe024f948e6e5be
Alen Jo <alenjo874@gmail.com>
2022-04-26 10:29:49 -0400

    insertion sort

---
+++ insertionSort.js
@@ -0,0 +1,13 @@
+var sortArray = function (nums) {
+  //inserstion sort
+
+  for (let i = 1; i < nums.length; i++) {
+    let j = i;
+    while (j > 0 && nums[j] < nums[j - 1]) {
+      [nums[j], nums[j - 1]] = [nums[j - 1], nums[j]];
+      j--;
+    }
+  }
+
+  return nums;
+};
