9f2fc63de511aea2997fdfb3ca07f8beb846b2da
Alen Jo <alenjo874@gmail.com>
2022-04-27 11:20:00 -0400

    find duplicate num

---
+++ findTheDuplicateNum.js
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/find-the-duplicate-number/submissions/
+// Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+// There is only one repeated number in nums, return this repeated number.
+// You must solve the problem without modifying the array nums and uses only constant extra space.
+
+//brute force
+var findDuplicate = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] === nums[j]) {
+        return nums[i];
+      }
+    }
+  }
+};
